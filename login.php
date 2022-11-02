b427f54f10a567d38b789bcf5eeedc861bab07c3
Jovana Stakić <jovana.stakic997@gmail.com>
2022-11-02 22:18:35 +0100

    Napravljena je login stranica

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Login Page</title>
+</head>
+<body >
+  <div class="login-form">
+    <form action="#" method="post">
+        <h2 class="text-center">Log in</h2>       
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="Korisničko ime" required="required">
+        </div>
+        <div class="form-group">
+            <input type="password" class="form-control" placeholder="Lozinka" required="required">
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn  btn-block">Log in</button>
+        </div>
+             
+    </form>
+
+</div>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</body>
+</html>
